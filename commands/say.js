49a9f322450d39da9cf5c9ab00b03b6ec02ab426
Paraffin Tutorials <parsa.firoozi20@gmail.com>
2021-11-29 16:06:26 +0330

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,10 +1,11 @@
-module.exports.run = async (app, message, args) => {
-
+module.exports.run = async (app, message, args) => 
+{
     message.delete();
     message.channel.send(args.join(" "))
 }
 
-module.exports.help = {
+module.exports.help = 
+{
     name : "say",
     aliases : ["s"]
 }
